7f522a18966b973c0fbdbc8d8793acb2ae648270
Carly Zimmerman <carlyzimmerman@Carlys-MacBook-Air.local>
2024-02-23 11:54:26 -0700

    completed red, blue and green components

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/green">Green</Link>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
+  );
+}
